b355e6f4c4934afc25455b034d30fd81e4e5d21b
tryingtokeepup <kl.hawaii@gmail.com>
2021-06-10 17:23:34 -0700

    finished most of mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -6,15 +6,15 @@ import axios from 'axios';
     https://api.github.com/users/<your name>
 */
 
-let gitHubData = {};
-axios
-  .get('https://api.github.com/users/tryingtokeepup')
-  .then((response) => {
-    gitHubData = response.data;
-    console.log(gitHubData);
-  })
-  .catch((error) => console.log(error))
-  .finally(console.log('data is coming through'));
+// let gitHubData = {};
+// axios
+//   .get('https://api.github.com/users/tryingtokeepup')
+//   .then((response) => {
+//     gitHubData = response.data;
+//     console.log(gitHubData);
+//   })
+//   .catch((error) => console.log(error))
+//   .finally(console.log('data is coming through'));
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -29,6 +29,23 @@ axios
     and append the returned markup to the DOM as a child of .cards
 */
 
+// first, get the data from Github
+// lets query select the .cards div
+
+const cardsDiv = document.querySelector('.cards');
+console.log(cardsDiv);
+const gitHubCardMaker = (user) => {
+  return axios
+    .get(`https://api.github.com/users/${user}`)
+    .then((response) => {
+      console.log(response.data);
+      const newCard = cardMaker(response.data);
+      cardsDiv.appendChild(newCard);
+    })
+    .catch('failed!!!!');
+};
+
+gitHubCardMaker('tryingtokeepup');
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -40,8 +57,10 @@ axios
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
+const followersArray = ['stepbystepdayo', 'bigknell', 'dustinmyers'];
+followersArray.forEach((follower) => {
+  gitHubCardMaker(follower);
+});
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -62,19 +81,42 @@ const followersArray = [];
     </div>
 */
 
-function gitHubCardMaker(gitHubData) {
+function cardMaker(gitHubData) {
   // first, create all elements using createElement
+
+  console.log('Hey, I got the GitHub Data, YOO!!!! ', gitHubData);
+
+  // console.log('this is my img, =>', gitHubData.avatar_url);
   const cardContainer = document.createElement('div');
   const img = document.createElement('img');
+  img.src = gitHubData.avatar_url;
   const cardInfoContainer = document.createElement('div');
   const nameH3 = document.createElement('h3');
+  // console.log('this is my name, =>', gitHubData.name);
+
+  nameH3.textContent = gitHubData.name;
   const usernameP = document.createElement('p');
+  usernameP.textContent = gitHubData.login;
   const locationP = document.createElement('p');
+  locationP.textContent = gitHubData.location || 'fantasy land';
   const profileP = document.createElement('p');
+  profileP.textContent = 'Profile:';
   const profileA = document.createElement('a');
+  profileA.href = gitHubData.html_url;
+  profileA.textContent = gitHubData.html_url;
+  profileA.target = '_blank';
   const followersP = document.createElement('p');
+  followersP.textContent = `followers: ${gitHubData.followers}`;
   const followingP = document.createElement('p');
+  followingP.textContent = `following: ${gitHubData.following}`;
+
   const bioP = document.createElement('p');
+
+  if (gitHubData.bio === null) {
+    bioP.textContent = 'I have no bio!';
+  } else {
+    bioP.textContent = `Bio: ${gitHubData.bio}`;
+  }
 
   // second, attach classes to the elements with classList
 
@@ -101,7 +143,7 @@ function gitHubCardMaker(gitHubData) {
   return cardContainer;
 }
 
-gitHubCardMaker({});
+cardMaker({});
 
 /*
   List of LS Instructors Github username's:
